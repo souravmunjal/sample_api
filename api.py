81694c1d95280e498c8fde10b033df50e0b71e65
souravmunjal <30770269+souravmunjal@users.noreply.github.com>
2020-07-10 18:33:25 +0530

    added maximum movie selection code

--- api.py
+++ api.py
@@ -15,9 +15,18 @@ def getTheMaximumProfit():
 		full_ls.append(ls);
 	#now we have to use simple activity selection code to get the Maximum Number of movies he can do
 	full_ls.sort();
-	print(full_ls);
+	#get the Maximum number of movies
+	start_time=-1;
+	end_time=-1;
+	movies_name=[];
+	print(full_ls)
+	for i in full_ls:
+		if end_time<int(i[1]):
+			movies_name.append(i[2]);
+			end_time=int(i[0])
+			start_time=int(i[1])
 	
-	return jsonify({"ans":str(json_)})
+	return jsonify({"ans":str(movies_name),"total_money":str(len(movies_name))+"crores"})
 
 
 if __name__ == '__main__':
